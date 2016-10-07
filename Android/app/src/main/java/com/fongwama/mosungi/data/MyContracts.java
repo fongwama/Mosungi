2c2f86b4db6238a3f669f16115cc6f3d2ee17a1f
steve111MV <unlivregratuit@gmail.com>
2016-10-07 07:36:19 +0100

    Edited Agenda DB Table structure (1st Edit) Added CATEGORIE field to Table Patient (bacause we are going to implement the possibility of creating patients groups)

--- Android/app/src/main/java/com/fongwama/mosungi/data/MyContracts.java
+++ Android/app/src/main/java/com/fongwama/mosungi/data/MyContracts.java
@@ -10,25 +10,29 @@ public class MyContracts
     public static class TablePatient implements BaseColumns {
 
         public static final String TABLE_NAME   = "patient";
-
         public static final String NOM          = "nom_patient";
         public static final String PRENOM       = "prenom_patient";
         public static final String SEXE         = "sexe_patient";
         public static final String DATE         = "date_patient";
         public static final String TELEPHONE    = "telephone_patient";
         public static final String CAS          = "cas_patient";
-
+        public static final String CATEGORIE    = "groupe_patient";
     }
 
     public static class TableAgenda implements BaseColumns {
 
-        public static final String TABLE_NAME   = "agenda";
+        public static final String TABLE_NAME = "agenda";
 
-        public static final String TITRE          = "libele_agenda";
+        public static final String TITRE      = "libele_agenda";
         public static final String MESSAGE    = "sms_agenda";
-        public static final String DATE_MILLISS   = "date_agenda";
+
+        //This represent the IDs of patients to whom a message will be sent.
+        public static final String MESSAGE_NUMBERS = "sms_destinations_agenda";
+
+        public static final String DATE_MILLISS  = "date_unix_agenda";
         public static final String DATE          = "date_human_agenda";
-        public static final String ID_PATIENT          = "id_patient_agenda";
+        public static final String ALARM_MUSIC_PATH  = "alarm_music_agenda";
+        public static final String ALARM_VOLUME  = "alarm_vol_agenda";
 
     }
 }
