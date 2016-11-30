7fdc8970a6dee6bbf613c5a2f0b08d637969a4ad
loulendo hyllarion <byrck.nk@gmail.com>
2016-11-30 17:12:51 +0100

    Added two methods: 1 to get the formated himan resadable date & the other to convert default 12H time to 24H (French speaking countries time).

--- Android/app/src/main/java/com/fongwama/mosungi/functions/MyFunctions.java
+++ Android/app/src/main/java/com/fongwama/mosungi/functions/MyFunctions.java
@@ -2,36 +2,34 @@ package com.fongwama.mosungi.functions;
 
 import android.support.design.widget.TextInputEditText;
 
+import java.text.SimpleDateFormat;
+
 /**
  * Created by Karl on 05/07/2016.
  */
 public class MyFunctions {
 
+    //make unix server timestamp readable(by humans) as normal date
+    public static String getReadableDate(long timeStamp){
+        SimpleDateFormat shortenedDate = new SimpleDateFormat("EEE\ndd\nMMM");
+        return shortenedDate.format(timeStamp);
+    }
+
+    public static String get24Hour(long timeStamp){
+
+        SimpleDateFormat hour12to24 = new SimpleDateFormat("HH");
+        return hour12to24.format(timeStamp);
+    }
+
     //Fonctio qui vérifie si ub=n EditText est vide ou pas
     public Boolean checkTextInputEditText(TextInputEditText textInputEditText)
     {
-        Boolean result = true;
-        if (textInputEditText.getText().toString().isEmpty())
-        {
-            result = false;
-        }
-
-        return result;
+        return textInputEditText.getText().toString().isEmpty() ? false: true;
     }
 
     public Boolean checkPhoneNumberFormat(TextInputEditText phoneNumberEditText)
     {
-        Boolean result = true;
-        if (phoneNumberEditText.getText().toString().length() == 9 )
-        {
-            //Vérifie si le numéro a bel et bien 9 chiffres
-
-        }
-        else
-        {
-            result = false;
-        }
-
-        return result;
+        //Vérifie si le numéro a bel et bien 9 chiffres
+        return phoneNumberEditText.getText().toString().length() == 9 ? true : false;
     }
 }
